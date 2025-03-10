7dc8f099b0df5847f2bb829e1e80c34dacd4b48f
betakbing251 <betakbing251@gmail.com>
2025-03-10 09:38:38 +0000

    Add 5.php

---
+++ src/5.php
@@ -0,0 +1,5 @@
+ <?php 
+$max = 100; //set max to 100
+$min = 0;   //set min to 0
+$result = rand($min,$max);   //generate a random number between $min and $max
+echo $result;   //output the result ?>
