f36b06ad133da96f74dc50ee50f90182ee6db0e2
betakbing251 <betakbing251@gmail.com>
2025-04-01 16:00:27 +0000

    Add 21.php

---
+++ src/21.php
@@ -0,0 +1,8 @@
+<?php
+// Assume $students is an array of student data with properties: name, score
+$students = [
+    ['name' => 'Alice', 'score' => 85],
+    ['name' => 'Bob', 'score' => 92],
+    // ... add more students as needed
+];
+?>
