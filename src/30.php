1655d66e95cb44908ff8502baeeffab60a574a26
betakbing251 <betakbing251@gmail.com>
2025-04-23 10:39:51 +0000

    Add 30.php

---
+++ src/30.php
@@ -0,0 +1,14 @@
+<?php
+// Example PHP code for generating random data
+$randomData = [
+    'name' => "Alice",
+    'age' => 28,
+    'city' => "New York",
+    'favoriteFood' => "Pizza",
+    'isStudent' => true
+];
+
+foreach ($randomData as $key => $value) {
+    echo "$key: $value\n";
+}
+?>
