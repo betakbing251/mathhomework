3787b8e5a92845382b45b3eebf27a7a02cd8bf84
betakbing251 <betakbing251@gmail.com>
2025-05-27 16:13:21 +0000

    Add 41.php

---
+++ src/41.php
@@ -0,0 +1,9 @@
+<?php
+// Define some variables and arrays here...
+$variables = array();
+$array1 = array(1, 2, 3);
+$array2 = array('a' => 4, 'b' => 5);
+
+// Generate a random array of numbers between 0 and 9 inclusive...
+random_array_between(0, 9, $array1);
+?>
